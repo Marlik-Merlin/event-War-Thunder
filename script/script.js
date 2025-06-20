a3646a24f406e1cd6c1d8d69f62bb2900322cdc9
Marlik <78642523+Marlik-Merlin@users.noreply.github.com>
2025-06-20 12:08:08 +0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,30 @@
+const countDownDate = new Date("Jul 1, 2025 00:00:00").getTime();
+
+const updateTimer = () => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+};
+
+
+const timerInterval = setInterval(updateTimer, 1000);
+updateTimer();
+window.addEventListener('resize', function() {
+    const header = document.querySelector('header');
+    if (window.innerWidth <= 768) {
+        header.style.padding = '0.5rem 5%';
+    } else {
+        header.style.padding = '1rem 5%';
+    }
+});
